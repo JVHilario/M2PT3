24c9f8f767d97f81de505bdbadb79782a5fc1e5b
JVHilario <jvHilario@mcm.edu.ph>
2025-01-28 15:20:21 +0800

    Add files via upload

---
+++ Part1.java
@@ -0,0 +1,13 @@
+public class Part1 {
+    public static void main(String[] args) {
+        String name = "Julian Virl A. Hilario";
+        int age = 18;
+        double height = 1.63;
+        boolean isStudent = true;
+ 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Is a Student? " + isStudent);
+    }
+}
